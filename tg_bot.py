a335856600fcc7d79968cfb0f3fbacb534e11f9f
ps6104062-art <ps6104062@gmail.com>
2026-09-11 03:01:54 +0500

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -19,7 +19,7 @@ from telethon.sessions import StringSession
 BOT_TOKEN        = "8873506485:AAGn-H-PISk0n9LmpZe5Efy_J9OYoafGbgg"
 API_ID           = 37658735
 API_HASH         = "728f6de622061878b84d9f843181d879"
-ADMIN_ID         = 8826396052
+ADMIN_ID         = 5688523575
 SUPPORT_USERNAME = "@tebyaobossal"   # ← твой юзернейм для кнопки поддержки
 DB_PATH          = "accounts.db"
 SESSIONS_DIR     = "sessions"
